f5a549e6a62b58af6aa0af668c0b174e42d3848c
zyl409214686 <409214686@qq.com>
2019-06-14 13:58:16 +0800

    v2.4.7 补缺文件

---
+++ guide/src/main/java/com/app/hubert/guide/constant/Gravity.java
@@ -0,0 +1,13 @@
+package com.app.hubert.guide.constant;
+
+public class Gravity {
+    public static final int LEFT = 1;
+    public static final int TOP = 2;
+    public static final int RIGHT = 3;
+    public static final int BOTTOM = 4;
+//    public static final int LEFT_TOP = 5;
+//    public static final int RIGHT_TOP = 6;
+//    public static final int LEFT_BOTTOM = 7;
+//    public static final int RIGHT_BOTTOM = 8;
+
+}
